3767b4ef38032df0e02fe7310386811e307750b8
pineauk <kpineau@upei.ca>
2015-06-23 22:48:02 -0300

    feat(users): implement controller and template

--- client/app/scripts/controllers/users.js
+++ client/app/scripts/controllers/users.js
@@ -8,10 +8,22 @@
  * Controller of the clientApp
  */
 angular.module('clientApp')
-  .controller('UsersCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('UsersCtrl', function ($scope, User) {
+    var bindData = function () {
+      User.query().$promise.then(function (result) {
+        $scope.users = result;
+      });
+      $scope.newUser = {};
+    };
+
+    bindData();
+
+    $scope.addUser = function () {
+      var user = $scope.newUser;
+console.log(user);
+      //User.save(user).$promise.then(function () {
+      //  bindData();
+      //});
+    };
+
   });
